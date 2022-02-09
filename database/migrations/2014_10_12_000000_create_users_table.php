33c09f6dddd925f5e24e72072e2b7f502e422b99
Zair <nurmukhamadov.z@future-group.ru>
2022-02-09 23:16:42 +0300

    migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('login')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('middle_name')->nullable();
             $table->rememberToken();
+            $table->timestamp('online_at')->nullable();
             $table->timestamps();
         });
     }
