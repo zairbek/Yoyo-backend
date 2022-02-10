b8d8f1560c3b8f90323c2dd5a9b5d0bcb7c6a905
Zair <nurmukhamadov.z@future-group.ru>
2022-02-10 12:17:56 +0300

    fix psalm

--- app/Ship/Middlewares/RedirectIfAuthenticated.php
+++ app/Ship/Middlewares/RedirectIfAuthenticated.php
@@ -2,9 +2,11 @@
 
 namespace App\Ship\Middlewares;
 
-use App\Providers\RouteServiceProvider;
+use App\Ship\Providers\RouteServiceProvider;
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
@@ -12,12 +14,12 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @param  string|null  ...$guards
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param  array<string|null>  ...$guards
+     * @return Response|RedirectResponse
      */
-    public function handle(Request $request, Closure $next, ...$guards)
+    public function handle(Request $request, Closure $next, ...$guards): Response|RedirectResponse
     {
         $guards = empty($guards) ? [null] : $guards;
 
