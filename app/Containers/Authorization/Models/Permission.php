f3e4f09013cc2ae41acb8bed60d8e165bd5af7f3
Zair Nurmukhamadov <zair.nur@Zairs-Mac-mini.local>
2022-02-10 10:17:35 +0300

    permission package

---
+++ app/Containers/Authorization/Models/Permission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Containers\Authorization\Models;
+
+use Spatie\Permission\Models\Permission as SpatiePermission;
+
+class Permission extends SpatiePermission
+{
+    //
+}
---
+++ app/Containers/Authorization/Models/Role.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Containers\Authorization\Models;
+
+use Spatie\Permission\Models\Role as SpatieRole;
+
+class Role extends SpatieRole
+{
+    //
+}
