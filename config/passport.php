37e88a2fbb9a58444e6ad72a478af501c688c01b
Zair <nurmukhamadov.z@future-group.ru>
2022-02-10 17:24:54 +0300

    .

--- config/passport.php
+++ config/passport.php
@@ -63,4 +63,9 @@ return [
         ],
     ],
 
+    'tokens' => [
+        'access_token_lifetime' => env('ACCESS_TOKEN_LIFETIME', 86400),
+        'refresh_token_lifetime' => env('REFRESH_TOKEN_LIFETIME', 86400 * 7),
+        'refresh_token_cache_domain' => env('REFRESH_TOKEN_COOKIE_DOMAIN', 'http://localhost'),
+    ],
 ];
