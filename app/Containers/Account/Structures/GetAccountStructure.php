e2cce6070882e85b6f17cb3c4abd59963db3408a
Zair <nurmukhamadov.z@future-group.ru>
2022-05-18 23:23:38 +0300

    update account repo

--- app/Containers/Account/Structures/GetAccountStructure.php
+++ app/Containers/Account/Structures/GetAccountStructure.php
@@ -7,8 +7,8 @@ use App\Ship\Core\Abstracts\Structures\Structure;
 class GetAccountStructure extends Structure
 {
     public int $id;
-    public string $login;
-    public string $email;
+    public string|null $login;
+    public string|null $email;
     public string|null $first_name;
     public string|null $last_name;
     public string|null $middle_name;
