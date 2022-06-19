a5cf086b004c1763b2f6b68e1d9f0127faa79d47
Zair <nurmukhamadov.z@future-group.ru>
2022-06-19 14:24:21 +0300

    avatar

--- app/Containers/User/Models/User.php
+++ app/Containers/User/Models/User.php
@@ -82,6 +82,13 @@ class User extends UserModel implements HasMedia
     ];
 
     /**
+     * @var string[]
+     */
+    protected $appends = [
+        'avatar'
+    ];
+
+    /**
      * The attributes that should be cast.
      *
      * @var array<string, string>
